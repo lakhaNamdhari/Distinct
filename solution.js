1588b81a73a161a3165ece29c0c7688574228220
Lakha Singh <polizic@yahoo.co.in>
2016-01-03 18:47:06 +0530

    added soltion with 100% at codelity

---
+++ solution.js
@@ -0,0 +1,19 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+
+// ToDo: Solve using Balanced BST
+function solution(A) {
+    // write your code in JavaScript (Node.js 4.0.0)
+    
+    var hash = {}, counter = 0, i;
+    
+    for ( i = 0; i < A.length; i++ ){
+        if ( hash[ A[i] ] === undefined ){
+            hash[ A[i] ] = 0;
+            counter++;
+        }
+    }
+    
+    return counter;
+}
